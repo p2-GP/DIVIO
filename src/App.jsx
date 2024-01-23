30911e77ce0c4b79045d0a4fe5752f6a6ec6070f
fikilifadly <fikilifadly@gmail.com>
2024-01-23 16:23:05 +0700

    feat: set basic video on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function randomID(len) {
+	let result = "";
+	if (result) return result;
+	var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+		maxPos = chars.length,
+		i;
+	len = len || 5;
+	for (i = 0; i < len; i++) {
+		result += chars.charAt(Math.floor(Math.random() * maxPos));
+	}
+	return result;
 }
 
-export default App
+export function getUrlParams(url = window.location.href) {
+	let urlStr = url.split("?")[1];
+	return new URLSearchParams(urlStr);
+}
+
+export default function App() {
+	const roomID = getUrlParams().get("roomID") || randomID(5);
+	let myMeeting = async (element) => {
+		// generate Kit Token
+		const appID = 1278173856;
+		const serverSecret = "bc36e0731b58235c238ec21248d5fcd4";
+		const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, randomID(5), randomID(5));
+
+		// Create instance object from Kit Token.
+		const zp = ZegoUIKitPrebuilt.create(kitToken);
+		// start the call
+		zp.joinRoom({
+			container: element,
+			sharedLinks: [
+				{
+					name: "Personal link",
+					url: window.location.protocol + "//" + window.location.host + window.location.pathname + "?roomID=" + roomID,
+				},
+			],
+			scenario: {
+				mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+			},
+		});
+	};
+
+	return <div className="myCallContainer" ref={myMeeting} style={{ width: "100vw", height: "100vh" }}></div>;
+}
